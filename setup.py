8e79fa611b5c67c1f52432692d1f93c328c4ff05
malinvaudpaul <paulmalinvaud@proton.me>
2022-11-16 10:17:00 +0100

    update requirements

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import find_packages
 setup(
     name="tdm-2022",
     version="1.0.0",
-    install_requires=["mypy", "pre-commit"],
+    install_requires=["mypy", "pre-commit", "pandas", "python-decouple"],
     extras_require={
         "arangodb": ["python-arango"],
         "janusgraph": [],
