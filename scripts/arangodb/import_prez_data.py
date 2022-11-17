bb39360605b5d7d50e4701f40b6ff0f689c84754
malinvaudpaul <paulmalinvaud@proton.me>
2022-11-17 18:57:47 +0100

    import data in arangodb with env variables almost ready

--- scripts/arangodb/import_prez_data.py
+++ scripts/arangodb/import_prez_data.py
@@ -1,43 +1,152 @@
 # -*- coding: utf-8 -*-
 from connection import client
+from decouple import config
+
+from data_treatment import clear_data, get_data
+
+path = config("PATH_TO_DATA")
+username = config("ARANGO_USERNAME")
+password = config("ARANGO_PASSWORD")
+database = config("ARANGO_DB_NAME")
+
+data = clear_data(get_data(path))
 
 # Connect to "_system" database as root user.
-sys_db = client.db("_system", username="root", password="Password")
+sys_db = client.db("_system", username=username, password=password)
 
-# Create a new database named "prez2017_1".
-sys_db.create_database("prez2017_1")
+# Create a new database named "tdm-2022".
+if not sys_db.has_database(database):
+    sys_db.create_database(database)
 
-# Connect to "prez2017_1" database as root user.
-db = client.db("prez2017_1", username="root", password="Password")
+# Connect to "tdm-2022" database as root user.
+db = client.db(database, username=username, password=password)
 
-# Create a new collection named "communes".
-communes = db.create_collection("communes")
+# Create a new graph named "prez-2017".
+graph = db.create_graph("prez-2017")
 
-# Add a hash index to the collection.
-communes.add_hash_index(fields=["libelle"], unique=True)
+# Create vertex collections for the graph.
+candidats = graph.create_vertex_collection("candidats")
+communes = graph.create_vertex_collection("communes")
 
-communes.insert({"name": "jane", "age": 39})
-communes.insert({"name": "josh", "age": 18})
-communes.insert({"name": "judy", "age": 21})
-
-# Create a new collection named "departements".
-departements = db.create_collection("departements")
-
-# Add a hash index to the collection.
-departements.add_hash_index(fields=["libelle"], unique=True)
+# Create an edge definition (relation) for the graph.
+edges = graph.create_edge_definition(
+    edge_collection="elit",
+    from_vertex_collections=["communes"],
+    to_vertex_collections=["candidats"],
+)
 
 
-# Create a new collection named "candidats".
-candidats = db.create_collection("candidats")
+candidats.insert({"_key": "EM", "full_name": "Emmanuel Macron"})
+candidats.insert({"_key": "MLP", "full_name": "Marine Le Pen"})
+candidats.insert({"_key": "NDA", "full_name": "Nicolas Dupont-Aignan"})
+candidats.insert({"_key": "JLM", "full_name": "Jean-Luc Mélenchon"})
+candidats.insert({"_key": "FF", "full_name": "Francois Fillon"})
+candidats.insert({"_key": "BH", "full_name": "Benoît Hamon"})
+candidats.insert({"_key": "NA", "full_name": "Nathalie Arthaud"})
+candidats.insert({"_key": "PP", "full_name": "Philippe Poutou"})
+candidats.insert({"_key": "FA", "full_name": "Francois Asselineau"})
+candidats.insert({"_key": "JC", "full_name": "Jacques Cheminade"})
+candidats.insert({"_key": "JL", "full_name": "Jean Lassalle"})
 
-candidats.insert({"name": "Le Pen", "firstname": "Marine"})
-candidats.insert({"name": "Dupont-Aignan", "firstname": "Nicolas"})
-candidats.insert({"name": "Macron", "firstname": "Emmanuel"})
-candidats.insert({"name": "Mélenchon", "firstname": "Jean-Luc"})
-candidats.insert({"name": "Hamon", "firstname": "Benoit"})
-candidats.insert({"name": "Arthaud", "firstname": "Nathalie"})
-candidats.insert({"name": "Poutou", "firstname": "Philippe"})
-candidats.insert({"name": "Cheminade", "firstname": "Jacques"})
-candidats.insert({"name": "Lassalle", "firstname": "Jean"})
-candidats.insert({"name": "Asselineau", "firstname": "Francois"})
-candidats.insert({"name": "Fillon", "firstname": "Francois"})
+for index, row in data.iterrows():
+    commune_key = f"D{row['Code du departement']}C{row['Code de la commune']}"
+    communes.insert(
+        {
+            "_key": commune_key,
+            "libelle_commune": row["Libelle de la commune"],
+            "code_commune": row["Code de la commune"],
+            "code_departement": row["Code du departement"],
+            "libelle_departement": row["Libelle du departement"],
+            "inscrits": row["Inscrits"],
+            "abstentions": row["Abstentions"],
+            "%Abs.Ins": row["%Abs.Ins"],
+            "votants": row["Votants"],
+            "%Vot.Ins": row["%Vot.Ins"],
+            "blancs": row["Blancs"],
+            "%Blancs.Ins": row["%Blancs.Ins"],
+            "%Blancs.Vot": row["%Blancs.Vot"],
+            "nuls": row["Nuls"],
+            "%Nuls.Ins": row["%Nuls.Ins"],
+            "%Nuls.Vot": row["%Nuls.Vot"],
+            "exprimes": row["Exprimes"],
+            "%Exp.Ins": row["%Exp.Ins"],
+            "%Exp.Vot": row["%Exp.Vot"],
+        }
+    )
+    edges.insert(
+        {
+            "_from": f"communes/{commune_key}",
+            "_to": "candidats/EM",
+            "score": row["MACRON Emmanuel"],
+        }
+    )
+    edges.insert(
+        {
+            "_from": f"communes/{commune_key}",
+            "_to": "candidats/MLP",
+            "score": row["LE PEN Marine"],
+        }
+    )
+    edges.insert(
+        {
+            "_from": f"communes/{commune_key}",
+            "_to": "candidats/NDA",
+            "score": row["DUPONT-AIGNAN Nicolas"],
+        }
+    )
+    edges.insert(
+        {
+            "_from": f"communes/{commune_key}",
+            "_to": "candidats/JLM",
+            "score": row["MELENCHON Jean-Luc"],
+        }
+    )
+    edges.insert(
+        {
+            "_from": f"communes/{commune_key}",
+            "_to": "candidats/FF",
+            "score": row["FILLON Francois"],
+        }
+    )
+    edges.insert(
+        {
+            "_from": f"communes/{commune_key}",
+            "_to": "candidats/BH",
+            "score": row["HAMON Benoit"],
+        }
+    )
+    edges.insert(
+        {
+            "_from": f"communes/{commune_key}",
+            "_to": "candidats/NA",
+            "score": row["ARTHAUD Nathalie"],
+        }
+    )
+    edges.insert(
+        {
+            "_from": f"communes/{commune_key}",
+            "_to": "candidats/PP",
+            "score": row["POUTOU Philippe"],
+        }
+    )
+    edges.insert(
+        {
+            "_from": f"communes/{commune_key}",
+            "_to": "candidats/FA",
+            "score": row["ASSELINEAU Francois"],
+        }
+    )
+    edges.insert(
+        {
+            "_from": f"communes/{commune_key}",
+            "_to": "candidats/JC",
+            "score": row["CHEMINADE Jacques"],
+        }
+    )
+    edges.insert(
+        {
+            "_from": f"communes/{commune_key}",
+            "_to": "candidats/JL",
+            "score": row["LASSALLE Jean"],
+        }
+    )
