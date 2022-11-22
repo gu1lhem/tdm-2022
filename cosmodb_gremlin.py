f6e6d9ac61124cf41df50505e2d0fda4183b980c
gu1lhem <sky@anche.no>
2022-11-22 15:25:38 +0100

    CDB vertices insertion

--- cosmodb_gremlin.py
+++ cosmodb_gremlin.py
@@ -14,11 +14,110 @@ from gremlin_python.driver import (  # This is the endpoint of the Gremlin serve
 from gremlin_python.driver.protocol import \
     GremlinServerError  # GremlinServerError is raised when the server returns an error.
 
+from data_treatment import clear_data, get_data
+
 path = config("PATH_TO_DATA")
 COSMODB_ENDPOINT = config("COSMODB_ENDPOINT")
 COSMODB_DATABASE = config("COSMODB_DATABASE")
 COSMODB_GRAPH = config("COSMODB_GRAPH")
 COSMODB_PASSWORD = config("COSMODB_PASSWORD")
+
+data = clear_data(get_data(path))
+
+print(data)
+
+"""
+data is like :
+[35719 rows x 29 columns]
+Code.du.departement 	Libelle.du.departement 	Code.de.la.commune 	Libelle.de.la.commune 	Inscrits 	Abstentions 	%Abs.Ins 	Votants 	%Vot.Ins 	Blancs 	%Blancs.Ins 	%Blancs.Vot 	Nuls 	%Nuls.Ins 	%Nuls.Vot 	Exprimes 	%Exp.Ins 	%Exp.Vot 	DUPONT-AIGNAN_Nicolas 	LE PEN_Marine 	MACRON_Emmanuel 	HAMON_Benoit 	ARTHAUD_Nathalie 	POUTOU_Philippe 	CHEMINADE_Jacques 	LASSALLE_Jean 	MELENCHON_Jean-Luc 	ASSELINEAU_Francois 	FILLON_Francois
+1 	Ain 	1 	L’Abergement-Clémenciat 	598 	92 	15.38 	506 	84.62 	2 	0.33 	0.4 	9 	1.51 	1.78 	495 	82.78 	97.83 	6.87 	25.45 	24.04 	5.86 	0.81 	0.81 	0.4 	0.4 	11.92 	1.21 	22.22
+1 	Ain 	2 	L’Abergement-de-Varey 	209 	25 	11.96 	184 	88.04 	6 	2.87 	3.26 	2 	0.96 	1.09 	176 	84.21 	95.65 	3.41 	27.27 	21.02 	7.39 	1.14 	1.14 	0 	0 	18.75 	0.57 	19.32
+1 	Ain 	4 	Ambérieu-en-Bugey 	8586 	1962 	22.85 	6624 	77.15 	114 	1.33 	1.72 	58 	0.68 	0.88 	6452 	75.15 	97.4 	5.36 	25.84 	20.64 	5.33 	0.62 	1.41 	0.08 	0.93 	21.88 	1.1 	16.8
+"""
+
+
+def create_insert_vertices_query(data):
+    """
+    Create the query to insert the vertices in the graph from the `data` DataFrame.
+    """
+    # We will create two collections : 'candidats' and 'communes'.
+    # The 'candidats' collection will contain the candidats.
+    # The 'communes' collection will contain the communes.
+    # The 'candidats' collection will be linked to the 'communes' collection with the 'voted_in' edge.
+
+    # We will create a list of queries to insert the vertices.
+    queries = []
+
+    # We will create a list of the candidats.
+    candidats_dict = {}
+    candidats_dict["EM"] = "MACRON Emmanuel"
+    candidats_dict["MLP"] = "LE PEN Marine"
+    candidats_dict["NDA"] = "DUPONT-AIGNAN Nicolas"
+    candidats_dict["JLM"] = "MELENCHON Jean-Luc"
+    candidats_dict["FF"] = "FILLON Francois"
+    candidats_dict["BH"] = "HAMON Benoit"
+    candidats_dict["NA"] = "ARTHAUD Nathalie"
+    candidats_dict["PP"] = "POUTOU Philippe"
+    candidats_dict["FA"] = "ASSELINEAU Francois"
+    candidats_dict["JC"] = "CHEMINADE Jacques"
+    candidats_dict["JL"] = "LASSALLE Jean"
+
+    # We will create a query to insert the candidats with the dict
+    for candidat in candidats_dict:
+        query = "g.addV('candidat').property('id', '{0}').property('full_name', '{1}')".format(
+            candidat, candidats_dict[candidat]
+        )
+        queries.append(query)
+
+    for index, row in data.iterrows():
+        commune_key = f"D{row['Code du departement']}C{row['Code de la commune']}"
+        query = "g.addV('commune').property('id', '{0}').property('libelle_commune', '{1}').property('code_commune', '{2}').property('code_departement', '{3}').property('libelle_departement', '{4}').property('inscrits', {5}).property('abstentions', {6}).property('pourcentage_abstentions', {7}).property('votants', {8}).property('pourcentage_votants', {9}).property('blancs', {10}).property('pourcentage_blancs', {11}).property('pourcentage_blancs_sur_votants', {12}).property('nuls', {13}).property('pourcentage_nuls', {14}).property('pourcentage_nuls_sur_votants', {15}).property('exprimes', {16}).property('pourcentage_exprimes', {17}).property('pourcentage_exprimes_sur_votants', {18})".format(
+            commune_key,
+            row["Libelle de la commune"],
+            row["Code de la commune"],
+            row["Code du departement"],
+            row["Libelle du departement"],
+            row["Inscrits"],
+            row["Abstentions"],
+            row["%Abs Ins"],
+            row["Votants"],
+            row["%Vot Ins"],
+            row["Blancs"],
+            row["%Blancs Ins"],
+            row["%Blancs Vot"],
+            row["Nuls"],
+            row["%Nuls Ins"],
+            row["%Nuls Vot"],
+            row["Exprimes"],
+            row["%Exp Ins"],
+            row["%Exp Vot"],
+        )
+        queries.append(query)
+
+    return queries
+
+
+_gremlin_insert_vertices = create_insert_vertices_query(data)
+
+
+def insert_vertices(client):
+    for query in _gremlin_insert_vertices:
+        print("\n> {0}\n".format(query))
+        callback = client.submitAsync(query)
+        if callback.result() is not None:
+            print(
+                "\tInserted this vertex:\n\t{0}".format(
+                    callback.result().all().result()
+                )
+            )
+        else:
+            print("Something went wrong with this query: {0}".format(query))
+        print("\n")
+        print_status_attributes(callback.result())
+        print("\n")
+
+    print("\n")
+
 
 if sys.platform == "win32":
     asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
@@ -28,12 +127,6 @@ if sys.platform == "win32":
 
 _gremlin_cleanup_graph = "g.V().drop()"
 
-_gremlin_insert_vertices = [
-    "g.addV('person').property('id', 'thomas').property('firstName', 'Thomas').property('age', 44).property('part', 'part')",
-    "g.addV('person').property('id', 'mary').property('firstName', 'Mary').property('lastName', 'Andersen').property('age', 39).property('part', 'part')",
-    "g.addV('person').property('id', 'ben').property('firstName', 'Ben').property('lastName', 'Miller').property('part', 'part')",
-    "g.addV('person').property('id', 'robin').property('firstName', 'Robin').property('lastName', 'Wakefield').property('part', 'part')",
-]
 
 _gremlin_insert_edges = [
     "g.V('thomas').addE('knows').to(g.V('mary'))",
@@ -82,25 +175,6 @@ def cleanup_graph(client):
         callback.result().all().result()
     print("\n")
     print_status_attributes(callback.result())
-    print("\n")
-
-
-def insert_vertices(client):
-    for query in _gremlin_insert_vertices:
-        print("\n> {0}\n".format(query))
-        callback = client.submitAsync(query)
-        if callback.result() is not None:
-            print(
-                "\tInserted this vertex:\n\t{0}".format(
-                    callback.result().all().result()
-                )
-            )
-        else:
-            print("Something went wrong with this query: {0}".format(query))
-        print("\n")
-        print_status_attributes(callback.result())
-        print("\n")
-
     print("\n")
 
 
