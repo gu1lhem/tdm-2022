1975642f8d42b53eb739813c157f478f8f779dca
gu1lhem <sky@anche.no>
2022-11-29 13:18:33 +0100

    CDB Cleanup

--- cosmodb_gremlin.py
+++ cosmodb_gremlin.py
@@ -55,6 +55,78 @@ candidats_dict = {
     "JL": "LASSALLE Jean",
 }
 
+_gremlin_cleanup_graph = "g.V().drop()"
+
+def cleanup_graph(client):
+    print("\n> {0}".format(_gremlin_cleanup_graph))
+    callback = client.submitAsync(_gremlin_cleanup_graph)
+    if callback.result() is not None:
+        callback.result().all().result()
+    print("\n")
+    print_status_attributes(callback.result())
+    print("\n")
+
+
+def print_status_attributes(result):
+    # This logs the status attributes returned for successful requests.
+    # See list of available response status attributes (headers) that Gremlin API can return:
+    #     https://docs.microsoft.com/en-us/azure/cosmos-db/gremlin-headers#headers
+    #
+    # These responses includes total request units charged and total server latency time.
+    #
+    # IMPORTANT: Make sure to consume ALL results returend by cliient tothe final status attributes
+    # for a request. Gremlin result are stream as a sequence of partial response messages
+    # where the last response contents the complete status attributes set.
+    #
+    # This can be
+    print("\tResponse status_attributes:\n\t{0}".format(result.status_attributes))
+
+
+
+try:
+    client = client.Client(
+        f"wss://{COSMODB_ENDPOINT}.gremlin.cosmos.azure.com:443/",
+        "g",
+        username=f"/dbs/{COSMODB_DATABASE}/colls/{COSMODB_GRAPH}",
+        password=f"{COSMODB_PASSWORD}",
+        message_serializer=serializer.GraphSONSerializersV2d0(),
+    )
+
+    print("Welcome to Azure Cosmos DB + Gremlin on Python!")
+
+    # Drop the entire Graph
+    input(
+        "We're about to drop whatever graph is on the server. Press any key to continue..."
+    )
+    cleanup_graph(client)
+except GremlinServerError as e:
+    print("Code: {0}, Attributes: {1}".format(e.status_code, e.status_attributes))
+
+    # GremlinServerError.status_code returns the Gremlin protocol status code
+    # These are broad status codes which can cover various scenaios, so for more specific
+    # error handling we recommend using GremlinServerError.status_attributes['x-ms-status-code']
+    #
+    # Below shows how to capture the Cosmos DB specific status code and perform specific error handling.
+    # See detailed set status codes than can be returned here: https://docs.microsoft.com/en-us/azure/cosmos-db/gremlin-headers#status-codes
+    #
+    # See also list of available response status attributes that Gremlin API can return:
+    #     https://docs.microsoft.com/en-us/azure/cosmos-db/gremlin-headers#headers
+    cosmos_status_code = e.status_attributes["x-ms-status-code"]
+    if cosmos_status_code == 409:
+        print("Conflict error!")
+    elif cosmos_status_code == 412:
+        print("Precondition error!")
+    elif cosmos_status_code == 429:
+        print("Throttling error!")
+    elif cosmos_status_code == 1009:
+        print("Request timeout error!")
+    else:
+        print("Default error handling")
+
+    traceback.print_exc(file=sys.stdout)
+    sys.exit(1)
+
+
 
 def create_insert_vertices_query(data):
     """
@@ -101,8 +173,34 @@ def create_insert_vertices_query(data):
 
     return queries
 
+try:
+    _gremlin_insert_vertices = create_insert_vertices_query(data)
+except GremlinServerError as e:
+    print("Code: {0}, Attributes: {1}".format(e.status_code, e.status_attributes))
 
-_gremlin_insert_vertices = create_insert_vertices_query(data)
+    # GremlinServerError.status_code returns the Gremlin protocol status code
+    # These are broad status codes which can cover various scenaios, so for more specific
+    # error handling we recommend using GremlinServerError.status_attributes['x-ms-status-code']
+    #
+    # Below shows how to capture the Cosmos DB specific status code and perform specific error handling.
+    # See detailed set status codes than can be returned here: https://docs.microsoft.com/en-us/azure/cosmos-db/gremlin-headers#status-codes
+    #
+    # See also list of available response status attributes that Gremlin API can return:
+    #     https://docs.microsoft.com/en-us/azure/cosmos-db/gremlin-headers#headers
+    cosmos_status_code = e.status_attributes["x-ms-status-code"]
+    if cosmos_status_code == 409:
+        print("Conflict error!")
+    elif cosmos_status_code == 412:
+        print("Precondition error!")
+    elif cosmos_status_code == 429:
+        print("Throttling error!")
+    elif cosmos_status_code == 1009:
+        print("Request timeout error!")
+    else:
+        print("Default error handling")
+
+    traceback.print_exc(file=sys.stdout)
+    sys.exit(1)
 
 
 def create_insert_edges_query(data):
@@ -122,8 +220,34 @@ def create_insert_edges_query(data):
             query = f"g.V('{commune_key}').addE('score').to(g.V('{candidat}')).property('score', {row[candidats_dict[candidat]]})"
             queries.append(query)
 
+try:
+    _gremlin_insert_edges = create_insert_edges_query(data)
+except GremlinServerError as e:
+    print("Code: {0}, Attributes: {1}".format(e.status_code, e.status_attributes))
 
-_gremlin_insert_edges = create_insert_edges_query(data)
+    # GremlinServerError.status_code returns the Gremlin protocol status code
+    # These are broad status codes which can cover various scenaios, so for more specific
+    # error handling we recommend using GremlinServerError.status_attributes['x-ms-status-code']
+    #
+    # Below shows how to capture the Cosmos DB specific status code and perform specific error handling.
+    # See detailed set status codes than can be returned here: https://docs.microsoft.com/en-us/azure/cosmos-db/gremlin-headers#status-codes
+    #
+    # See also list of available response status attributes that Gremlin API can return:
+    #     https://docs.microsoft.com/en-us/azure/cosmos-db/gremlin-headers#headers
+    cosmos_status_code = e.status_attributes["x-ms-status-code"]
+    if cosmos_status_code == 409:
+        print("Conflict error!")
+    elif cosmos_status_code == 412:
+        print("Precondition error!")
+    elif cosmos_status_code == 429:
+        print("Throttling error!")
+    elif cosmos_status_code == 1009:
+        print("Request timeout error!")
+    else:
+        print("Default error handling")
+
+    traceback.print_exc(file=sys.stdout)
+    sys.exit(1)
 
 
 def insert_vertices(client):
@@ -147,7 +271,7 @@ def insert_vertices(client):
 
 
 
-_gremlin_cleanup_graph = "g.V().drop()"
+
 
 """
 _gremlin_update_vertices = ["g.V('thomas').property('age', 45)"]
@@ -171,47 +295,6 @@ _gremlin_drop_operations = {
 
 
 
-def print_status_attributes(result):
-    # This logs the status attributes returned for successful requests.
-    # See list of available response status attributes (headers) that Gremlin API can return:
-    #     https://docs.microsoft.com/en-us/azure/cosmos-db/gremlin-headers#headers
-    #
-    # These responses includes total request units charged and total server latency time.
-    #
-    # IMPORTANT: Make sure to consume ALL results returend by cliient tothe final status attributes
-    # for a request. Gremlin result are stream as a sequence of partial response messages
-    # where the last response contents the complete status attributes set.
-    #
-    # This can be
-    print("\tResponse status_attributes:\n\t{0}".format(result.status_attributes))
-
-
-def cleanup_graph(client):
-    print("\n> {0}".format(_gremlin_cleanup_graph))
-    callback = client.submitAsync(_gremlin_cleanup_graph)
-    if callback.result() is not None:
-        callback.result().all().result()
-    print("\n")
-    print_status_attributes(callback.result())
-    print("\n")
-
-
-def insert_edges(client):
-    for query in _gremlin_insert_edges:
-        print("\n> {0}\n".format(query))
-        callback = client.submitAsync(query)
-        if callback.result() is not None:
-            print(
-                "\tInserted this edge:\n\t{0}\n".format(
-                    callback.result().all().result()
-                )
-            )
-        else:
-            print("Something went wrong with this query:\n\t{0}".format(query))
-        print_status_attributes(callback.result())
-        print("\n")
-
-    print("\n")
 
 
 def update_vertices(client):
@@ -320,31 +403,7 @@ just_run(client, _gremlin_query_6)
 
 
 try:
-    client = client.Client(
-        f"wss://{COSMODB_ENDPOINT}.gremlin.cosmos.azure.com:443/",
-        "g",
-        username=f"/dbs/{COSMODB_DATABASE}/colls/{COSMODB_GRAPH}",
-        password=f"{COSMODB_PASSWORD}",
-        message_serializer=serializer.GraphSONSerializersV2d0(),
-    )
 
-    print("Welcome to Azure Cosmos DB + Gremlin on Python!")
-
-    # Drop the entire Graph
-    input(
-        "We're about to drop whatever graph is on the server. Press any key to continue..."
-    )
-    cleanup_graph(client)
-
-    # Insert all vertices
-    input("Let's insert some vertices into the graph. Press any key to continue...")
-    insert_vertices(client)
-
-    # Create edges between vertices
-    input(
-        "Now, let's add some edges between the vertices. Press any key to continue..."
-    )
-    insert_edges(client)
     """
     # Update a vertex
     input(
@@ -369,7 +428,7 @@ try:
     # Count all vertices again
     input("How many vertices do we have left? Press any key to continue...")
     count_vertices(client)
-    """
+    
 
     # We will make the requests we decided to do:
 
@@ -380,6 +439,8 @@ try:
     callback = client.submitAsync(query)
     for result in callback.result():
         print("\t{0}".format(str(result)))
+
+    """
 
 
 except GremlinServerError as e:
